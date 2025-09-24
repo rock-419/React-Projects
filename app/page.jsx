4a6f692f3f1f868452085b59bdbe87faa55ff642
rock <chuluudai.lkh@gmail.com>
2025-09-24 20:39:30 +0800

    added portfolio

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import About from "@/components/about";
 import Skills from "@/components/skills";
 
-export default function Home (){
-  return(
+export default function Home() {
+  return (
     <div>
-      <secton id="about"><About/></secton>
-      <secton id="skills"><Skills/></secton>
+      <secton id="about">
+        <About />
+      </secton>
+      <secton id="skills">
+        <Skills />
+      </secton>
     </div>
   );
 }
